63df0c35fe33638f92c5b61844a44bbd38992e53
Jethro Ordaneza <jet.oneza@gmail.com>
2016-09-06 02:12:35 +0800

    Add countdown timer

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,22 +1,63 @@
 var bgpage = chrome.extension.getBackgroundPage();
+var refresh = true;
 
 function set() {
-  bgpage.setTimer();
+  bgpage.startTimer();
+  refresh = true;
+  setActions();
   refreshDisplay();
+}
+
+function stop() {
+  bgpage.stopTimer();
+  setActions();
+  updateDisplay();
+  refresh = false;
+}
+
+function formatDigit(num) {
+  return num.toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping: false});
+}
+
+function formatTime(time) {
+  var min = Math.floor(time / 60);
+  var sec = time % 60;
+  return formatDigit(min) + ":" + formatDigit(sec);
 }
 
 function refreshDisplay()
 {
+  updateDisplay();
+  if(refresh) {
+   refreshDisplayTimeout = setTimeout(refreshDisplay, 100);
+  }
+}
+
+function updateDisplay() {
   var time = bgpage.getTime();
-  document.querySelector('.time').innerHTML = time;
-  refreshDisplayTimeout = setTimeout(refreshDisplay, 100);
+  document.querySelector('.time').innerHTML = formatTime(time);
 }
 
 function load() {
+  setActions();
   refreshDisplay();
+}
+
+function setActions() {
+  var isRunning = bgpage.isRunning();
+  document.querySelector('.btn-set').style.display = isRunning ? "none" : "inline-block";
+  document.querySelector('.time-select').style.display = isRunning ? "none" : "inline-block";
+  document.querySelector('.btn-stop').style.display = isRunning ? "block" : "none";
+}
+
+function select(e) {
+  bgpage.setTime(parseInt(e.target.value));
+  updateDisplay();
 }
 
 document.addEventListener('DOMContentLoaded', function() {
   load();
   document.querySelector('.btn-set').addEventListener('click', set);
+  document.querySelector('.btn-stop').addEventListener('click', stop);
+  document.querySelector('.time-select').addEventListener('change', select);
 });
--- scripts/timer.js
+++ scripts/timer.js
@@ -1,14 +1,45 @@
-var time = 0;
-var timer = null;
+var SEC = 60;
+var DEFAULT_MIN = 5;
 
-function setTimer() {
-  timer = setInterval(updateTime, 1000);
+var defaultState = {
+  selectedTime: DEFAULT_MIN * SEC,
+  time: DEFAULT_MIN * SEC,
+  timer: null,
+  running: false
+}
+
+var state = defaultState;
+
+function startTimer() {
+  state.running = true;
+  state.timer = setInterval(updateTime, 1000);
+}
+
+function stopTimer() {
+  state.running = false;
+  clearInterval(state.timer);
+  state.timer = null;
+  state.time = state.selectedTime;
 }
 
 function updateTime() {
-  time++;
+  state.time--;
+  if(state.time == 0) {
+    stopTimer();
+    state.time = state.selectedTime;
+    return;
+  }
 }
 
 function getTime() {
-  return time;
+  return state.time;
+}
+
+function setTime(min) {
+  state.time = min * SEC;
+  state.selectedTime = state.time;
+}
+
+function isRunning() {
+  return state.running;
 }
